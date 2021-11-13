19dc82ac00f45ae898e4148b8f4cb782b1ced633
jubairzaman <Jubair.zaman01@gmail.com>
2021-11-13 23:20:19 +0600

    more

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Chaka server is running");
+  res.send("SuperWheels server is running");
 });
 
 app.listen(port, () => {
