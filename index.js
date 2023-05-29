194871725692311e4e4a6ea4617dfa92acfab70c
jubairzaman <jubair.zaman01@gmail.com>
2023-05-29 18:20:02 +0600

    server update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ admin.initializeApp({
 });
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6e2lo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://Car-Server:rdjwdxxgoDts76yn@cluster0.6e2lo.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
